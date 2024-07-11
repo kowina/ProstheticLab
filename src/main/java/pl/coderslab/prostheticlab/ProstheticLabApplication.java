821d33ae5490257e269bf90f84129bf391927185
twój login do GitHuba <kowina@gazeta.pl>
2024-07-11 22:29:23 +0200

    first commit

---
+++ src/main/java/pl/coderslab/prostheticlab/ProstheticLabApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.prostheticlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProstheticLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProstheticLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/coderslab/prostheticlab/ProstheticLabApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.prostheticlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProstheticLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
