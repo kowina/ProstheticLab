9a097735d52cc2f157e0d358fe45a1152f1890ba
twój login do GitHuba <kowina@gazeta.pl>
2024-07-17 16:58:48 +0200

    entities offer and work item

---
+++ src/main/java/pl/coderslab/prostheticlab/domain/Offer.java
@@ -0,0 +1,61 @@
+package pl.coderslab.prostheticlab.domain;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "offers")
+public class Offer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 2, message = "nazwa musi mieć przynajmniej 2 znaki")
+    private String name;
+
+    @NotBlank
+    @Min(value = 1, message = "cena musi być wieksza od 0")
+    @Column(name = "unit_price")
+    private int unitPrice;
+
+    public Offer() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(int unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Offer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", unitPrice=" + unitPrice +
+                '}';
+    }
+}
